5074deee145cf100d6b34a0cca9029985d50b1fb
Parker Ensing <parker.a.ensing@gmail.com>
2026-04-11 23:44:42 -0400

    clerical fix to EPD_init.cpp

--- Sketches/spi_testbed/EPD_init.cpp
+++ Sketches/spi_testbed/EPD_init.cpp
@@ -27,7 +27,7 @@ void EPD_HW_RESET(void) {
 
 void EPD_Update(void) {
     DISP_SPI_WRITE_COMMAND(0x22); // Command: Display Update Control 2
-    DISP_SPI_WRITE_DATA(0xF7); // operating sequence parameter
+    DISP_SPI_WRITE_DATA(0xF7); // operating sequence parameter (should probably be 0xFF)
     DISP_SPI_WRITE_COMMAND(0x20); // Command: Master Activation
     EPD_BUSY_HOLD();
 }
@@ -36,14 +36,42 @@ void EPD_Update(void) {
 // Note: works in local brush mode
 void EPD_PartialUpdate(void) {
     DISP_SPI_WRITE_COMMAND(0x22); // Command: Display Update Control 2
-    DISP_SPI_WRITE_DATA(0xDC); // operating sequence parameter
+    DISP_SPI_WRITE_DATA(0xDC); // operating sequence parameter (Don't know what this does)
     DISP_SPI_WRITE_COMMAND(0x20); // Command: Master Activation
     EPD_BUSY_HOLD();
 }
 
+// Update with faster operating sequence but no temp data or LUT
+// Note: works in quick brush mode
 void EPD_FastUpdate(void) {
     DISP_SPI_WRITE_COMMAND(0x22); // Command: Display Update Control 2
-    DISP_SPI_WRITE_DATA(0xC7); // operating sequence parameter
+    DISP_SPI_WRITE_DATA(0xC7); // operating sequence parameter (should probably be 0xCF)
     DISP_SPI_WRITE_COMMAND(0x20); // Command: Master Activation
     EPD_BUSY_HOLD();
 }
+
+// Place the display in a low power mode
+void EPD_DeepSleep(void){
+    DISP_SPI_WRITE_COMMAND(0x10); // Command: Deep Sleep Mode
+    DISP_SPI_WRITE_DATA(0x01); // enter deep sleep mode 1
+    delay(5);
+}
+
+// Initialize the display by resetting the hardware and then software
+void EPD_Init(void) {
+    EPD_HW_RESET();
+    EPD_BUSY_HOLD();
+    DISP_SPI_WRITE_COMMAND(0x12); // Command: SW Reset
+    EPD_BUSY_HOLD();
+}
+
+
+void EPD_FastMode1Init(void);
+void EPD_SetRAMMP(void);
+void EPD_SetRAMMA(void);
+void EPD_SetRAMSP(void);
+void EPD_SetRAMSA(void);
+void EPD_Clear_R26A6H(void);
+void EPD_Display_Clear(void);
+void EPD_Display(const uint8_t *ImageBW);
+void EPD_WhiteScreen_ALL_Fast(const unsigned char *data);
