fdaaf0f34b6639dfae2b126cb0553caf4d6243c4
Parker Ensing <parker.a.ensing@gmail.com>
2026-05-09 19:34:09 -0400

    Adds epaper driver unit test

---
+++ Epaper Display/test/displays/drivers/test_epaper_display.cpp
@@ -0,0 +1,16 @@
+#include <unity.h>
+
+void setUp(void) {
+    // set stuff up here
+}
+
+
+void tearDown(void) {
+    // clean stuff up here
+}
+
+int main() {
+    UNITY_BEGIN();
+
+    UNITY_END();
+}
