6457372bf3ca183141cc3442e75e33c4fe47fa02
Parker Ensing <parker.a.ensing@gmail.com>
2026-06-14 17:17:51 -0400

    Adds lv_conf.h which somehow went missing

---
+++ Epaper Display/include/lv_conf.h
@@ -0,0 +1,13 @@
+#ifndef LV_CONF_H
+#define LV_CONF_H
+
+/*
+    Minimal LVGL config for PlatformIO bring-up.
+
+    For now, rely mostly on LVGL's defaults.
+    Add feature-specific config here later as needed.
+*/
+
+#define LV_USE_LOG 0
+
+#endif
