52f2e63e966118b0c4644f0f0e0a3cdd0b09cdc6
Parker Ensing <parker.a.ensing@gmail.com>
2026-08-16 00:00:24 -0400

    Changes body label font size

--- Epaper Display/src/main.cpp
+++ Epaper Display/src/main.cpp
@@ -5,11 +5,6 @@
 #include "displays/drivers/EPaperDisplay.h"
 #include "displays/ui/UIRuntime.h"
 
-// #include "displays/ui/screens/OrgSplashScreen.h"
-// #include "displays/ui/screens/ProductSplashScreen.h"
-// #include "displays/ui/screens/BootScreen.h"
-// #include "displays/ui/screens/NametagScreen.h"
-
 #include "services/AppContext.h"
 #include "services/apps/DiagnosticApp.h"
 #include "services/apps/UptimeApp.h"
@@ -18,11 +13,11 @@
 EPaperDisplay EPD;
 
 static AppContext appContext;
-static UptimeApp uptimeApp;
-// static DiagnosticApp diagnosticApp;
+// static UptimeApp uptimeApp;
+static DiagnosticApp diagnosticApp;
 
-static App* activeApp = &uptimeApp;
-// static App* activeApp = &diagnosticApp;
+// static App* activeApp = &uptimeApp;
+static App* activeApp = &diagnosticApp;
 
 
 static void processAppContext() {
@@ -33,35 +28,17 @@ static void processAppContext() {
     }
 }
 
+
 void setup() {
     EPD.begin();
     UIRuntime::begin(&EPD);
 
     activeApp->onEnter(appContext);
     processAppContext();
-
-    // lv_screen_load(OrgSplashScreen::create());
-    // UIRuntime::commit();
-    // delay(1000);
-
-    // lv_screen_load(ProductSplashScreen::create());
-    // UIRuntime::commit();
-    // delay(1000);
-
-    // lv_screen_load(BootScreen::create());
-    // UIRuntime::commit();
-
-    // lv_screen_load(NametagScreen::create());
-    // UIRuntime::commit();
-    // delay(5000);
-
-    // EPD.end();
 }
 
 
 void loop() {
-    // UIRuntime::service();
-    // delay(5);
     const uint32_t nowMs = millis();
     UIRuntime::service();
 
